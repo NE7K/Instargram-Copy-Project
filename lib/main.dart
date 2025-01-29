a8d6424e9df3853b2052e166eee93c9cdc14f7d2
NE7K <sjjang16@naver.com>
2025-01-30 03:00:59 +0900

    Finish sever get

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import './style.dart' as style; // style에서 파일 가져오겠읍니다
 import 'package:http/http.dart' as http; // http import, permission
 import 'dart:convert';
 
+import 'package:flutter/rendering.dart'; // scroll 관련 import
+
 void main() {
   runApp(
       MaterialApp(
@@ -26,6 +28,7 @@ class _MyAppState extends State<MyApp> {
 
   getData() async {
     var result = await http.get(Uri.parse('https://codingapple1.github.io/app/data.json'));
+    // 데이터 전송을 확인하려면 if statcode 200이런거 사용할 수 있음
     var result2 = jsonDecode(result.body);
     setState(() {
       data = result2;
@@ -55,7 +58,7 @@ class _MyAppState extends State<MyApp> {
             IconButton(onPressed: () {}, icon: Icon(Icons.add_box_outlined)), // 릴스, 게시물, 스토리 등등 활동 추가
             IconButton(onPressed: () {}, icon: Icon(Icons.menu)) ]  // 설정 및 활동
       ),
-      body: [HomePage(data : data), Text('검색창')][tab], // list 형식으로 간단하게 페이지 할당
+      body: [HomePage(data : data), Text('검색창')][tab], // list 형식으로 간단하게 페이지 할당, future builder 사용해서 if 사용 안 할 수 있음
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false, // tab 눌렀을 때 라벨 글자 나오지 않게 설정
         showUnselectedLabels: false,
@@ -84,17 +87,23 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemCount: 3, itemBuilder: (c, i) {
-      return Column( // return이 왜 존재해야 하는지?
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image.network(data[i]['image']),
-          Text(data[i]['likes'].toString()),
-          Text('글쓴이'),
-          Text('글내용')
-        ],
-      );
-    });
+
+    if(data.isnotEmpty) {
+      return ListView.builder(itemCount: 3, itemBuilder: (c, i) {
+        return Column( // return이 왜 존재해야 하는지?
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(data[i]['image']),
+            Text(data[i]['likes'].toString()),
+            Text('글쓴이'),
+            Text('글내용')
+          ],
+        );
+      });
+    }
+    else {
+      return Text('로딩중');
+    }
   }
 }
 
