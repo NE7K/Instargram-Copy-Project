bdde019c3f064bf8a2eebe7e0113512f9852d55b
NE7K <sjjang16@naver.com>
2025-02-02 04:08:19 +0900

    Firebase database get, post

--- lib/shop.dart
+++ lib/shop.dart
@@ -11,8 +11,24 @@ class Shop extends StatefulWidget {
 
 class _ShopState extends State<Shop> {
 
-  getData() {
+  getData() async {
+    // var result = await firestore.collection('product').doc('wgKCJb2zV22c0D3hcfIu').get();
+    // print(result);
+    // print(result['name']);
+    // if (result2.docs.isNotEmpty) {
 
+      var result2 = await firestore.collection('product').get(); // where 조건에 맞는 데이터 (쿼리), delete 삭제 가능, update 업데이트
+      for (var doc in result2.docs) {
+        print(doc['name']);
+    }
+      // awit firestore.collection().add({'name' : '내복'~~});
+    // 저장이 실패되거나 인터넷이 끊길때를 방지하기 위해서 try catch or if 사용
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
   }
 
   @override
