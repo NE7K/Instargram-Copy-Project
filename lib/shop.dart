a0d408aa5463704b1cba131b4f1a3657f3ebe5c2
NE7K <sjjang16@naver.com>
2025-02-01 14:00:10 +0900

    Firestore set

--- lib/shop.dart
+++ lib/shop.dart
@@ -5,12 +5,16 @@ final firestore = FirebaseFirestore.instance; // firestore instance 생성
 
 class Shop extends StatefulWidget {
   const Shop({super.key});
-
   @override
   State<Shop> createState() => _ShopState();
 }
 
 class _ShopState extends State<Shop> {
+
+  getData() {
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Text('샵 페이지임');
