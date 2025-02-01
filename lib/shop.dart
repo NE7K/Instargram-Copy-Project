4137cebaf18acc706c668cb4c6ba37702d76b005
NE7K <sjjang16@naver.com>
2025-02-02 04:24:20 +0900

    Added
    Login, Logout, Register,

--- lib/shop.dart
+++ lib/shop.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart'; // firestore import
 
+import 'package:firebase_auth/firebase_auth.dart'; // login
+
+final auth = FirebaseAuth.instance; // login instance
+
 final firestore = FirebaseFirestore.instance; // firestore instance 생성
 
 class Shop extends StatefulWidget {
@@ -25,10 +29,46 @@ class _ShopState extends State<Shop> {
     // 저장이 실패되거나 인터넷이 끊길때를 방지하기 위해서 try catch or if 사용
   }
 
+  registerUser() async {
+    try {
+      var result = await auth.createUserWithEmailAndPassword( // login은 signinwith~
+          email: 'kim@test.com',
+          password: '123456'
+      );
+      result.user?.updateDisplayName('john'); // ? or !
+      print(result.user);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  loginUser() async {
+    try {
+      await auth.signInWithEmailAndPassword(
+          email: 'kim@test.com',
+          password: '123456'
+      );
+    } catch (e) {
+      print(e);
+    }
+    if(auth.currentUser?.uid == null){
+      print('로그인 안된 상태군요');
+    } else {
+      print('로그인 하셨네');
+    }
+  }
+
+  logOut() async {
+    await auth.signOut();
+  }
+
+
   @override
   void initState() {
     super.initState();
     getData();
+    registerUser();
+    loginUser();
   }
 
   @override
